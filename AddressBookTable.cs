e0d058c156fc74990735de55974f4a0276f07930
mandara11 <mandaram2345@gmail.com>
2022-09-07 22:06:37 +0530

    AddressBookSystemLinQ

---
+++ AddressBookTable.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemLinQ
+{
+    public class AddressBookTable
+    {
+        // UC1:-Create a new DataTable
+        DataTable table = new DataTable("AddressBookSystem");
+
+        /* UC2:-Ability to create a Address Book Table with first and last names, address, city, 
+                 state, zip, phone number and email as its attributes.*/
+        public AddressBookTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));   //add columns
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+            table.Columns.Add("BookName", typeof(string));     //  UC9:- Ability to identify each Address Book with name and Type.
+            table.Columns.Add("BookType", typeof(string));
+
+       //UC3:- Ability to insert new Contacts to Address Book
+            table.Rows.Add("Mandara", "M", "Koppa, Patrahi", "Banglore", "Karnataka", "577101", "9977665543", "mandaram@gmail.com", "address001", "Friend");
+            table.Rows.Add("Vishnu", "Verma", "Banglore", "Banglr", " Karnataka", "577102", "8570934858", "ekta@gmail.com", "address002", "Friend");
+            table.Rows.Add("Vishal", "Singh", "Bng", "Banglore", " Karnataka", "577103", "7894561230", "vishal@gmail.com", "address002", "Friend");
+            table.Rows.Add("Varun", "Raj", "Banglore", "Banglr", "Karnataka", "577101", "7894561230", "varun@gmail.com", "address001", "Family");
+        }
+
+        public void GetAllContacts()
+        {
+            try
+            {
+                foreach (DataRow dr in table.AsEnumerable())
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("FirstName:- " + dr.Field<string>("firstName"));
+                    Console.WriteLine("LastName:- " + dr.Field<string>("lastName"));
+                    Console.WriteLine("Address:- " + dr.Field<string>("address"));
+                    Console.WriteLine("City:- " + dr.Field<string>("city"));
+                    Console.WriteLine("State:- " + dr.Field<string>("state"));
+                    Console.WriteLine("Zip:- " + dr.Field<string>("zip"));
+                    Console.WriteLine("PhoneNumber:- " + dr.Field<string>("phoneNumber"));
+                    Console.WriteLine("Email:- " + dr.Field<string>("eMail"));
+                    Console.WriteLine("BookName:- " + dr.Field<string>("bookName"));     // Newly added 
+                    Console.WriteLine("BookType:- " + dr.Field<string>("bookType"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.HelpLink);
+            }
+        }
+
+        //UC4:- Ability to edit existing contact person using their name
+        public void EditExistingContact(string firstName, string lastName, string column, string newValue)
+        {                                                                   // FirstOrDefault comes from System.Linq Namespace
+            DataRow contact = table.Select("FirstName = '" + firstName + "' and LastName = '" + lastName + "'").FirstOrDefault();
+            contact[column] = newValue;
+            Console.WriteLine("Record successfully Edited");
+            GetAllContacts();
+        }
+
+        //UC5:- Ability to delete a person using person's name.
+        public void DeleteContact(string firstName, string lastName)
+        {
+            try
+            {
+                DataRow contact = table.Select("FirstName = '" + firstName + "' and LastName = '" + lastName + "'").FirstOrDefault();
+                table.Rows.Remove(contact);
+                Console.WriteLine("Record Successfully Deleted");
+                GetAllContacts();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //UC6:- Ability to Retrieve Person belonging to a City or State from the Address Book.
+        public void RetrieveByCityOrState(string city, string state)
+        {
+            var retrieveData = from records in table.AsEnumerable()
+                               where records.Field<string>("City") == city || records.Field<string>("State") == state
+                               select records;
+            //Printing data
+            Console.WriteLine("\nRetrieve contact details by city or state name");
+            foreach (DataRow dr in table.AsEnumerable())
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("firstName"));
+                Console.WriteLine("LastName:- " + dr.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("address"));
+                Console.WriteLine("City:- " + dr.Field<string>("city"));
+                Console.WriteLine("State:- " + dr.Field<string>("state"));
+                Console.WriteLine("Zip:- " + dr.Field<string>("zip"));
+                Console.WriteLine("PhoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("Email:- " + dr.Field<string>("eMail"));
+            }
+        }
+
+        /*UC7:- Ability to understand the size of address book by City and State 
+              - Here size indicates the count.*/
+        public void CountByCityOrState(string city, string state)
+        {
+            var contact = from c in table.AsEnumerable()
+                          where c.Field<string>("City") == city && c.Field<string>("State") == state
+                          select c;
+
+            Console.WriteLine("Count of contacts in {0}, {1} is {2}", city, state, contact.Count());
+        }
+
+        //UC8:- Ability to retrieve entries sorted alphabetically by Person’s name for a given city.
+        public void SortedContactsByNameForAgivenCity(string City)
+        {
+            Console.WriteLine("Sorting by name for City");
+            var retrievedData = from records in table.AsEnumerable()
+                                where records.Field<string>("City") == City
+                                orderby records.Field<string>("FirstName"), records.Field<string>("LastName")
+                                select records;
+            ///Print Data
+            foreach (var dr in retrievedData)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("firstName"));
+                Console.WriteLine("LastName:- " + dr.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("address"));
+                Console.WriteLine("City:- " + dr.Field<string>("city"));
+                Console.WriteLine("State:- " + dr.Field<string>("state"));
+                Console.WriteLine("Zip:- " + dr.Field<string>("zip"));
+                Console.WriteLine("PhoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("Email:- " + dr.Field<string>("eMail"));
+            }
+        }
+
+        //UC10:- Ability to get number of contact persons i.e. count by type.
+        public void GetCountByAddressBookType()
+        {
+            var countData = table.AsEnumerable().GroupBy(BookType => BookType.Field<string>("BookType")).
+                Select(BookType => new
+                {
+                    BookType = BookType.Key,
+                    BookTypeCount = BookType.Count()
+                });
+            foreach (var contactlist in countData)
+            {
+                Console.WriteLine("AddressBook Type =" + contactlist.BookType + " --> " + "AddressBook_Count = " + contactlist.BookTypeCount);
+            }
+        }
+
+        //Uc11 Ability to retrieve entries sorted alphabetically by Person’s name for a given BookType
+        public void RetrieveByUsingBookType(string bookType)
+        {
+            var retrievedData = from records in table.AsEnumerable()
+                                where records.Field<string>("BookType") == bookType
+                                orderby records.Field<string>("FirstName"), records.Field<string>("LastName")
+                                select records;
+            foreach (var dr in retrievedData)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("firstName"));
+                Console.WriteLine("LastName:- " + dr.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("address"));
+                Console.WriteLine("City:- " + dr.Field<string>("city"));
+                Console.WriteLine("State:- " + dr.Field<string>("state"));
+                Console.WriteLine("Zip:- " + dr.Field<string>("zip"));
+                Console.WriteLine("PhoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("Email:- " + dr.Field<string>("eMail"));
+                Console.WriteLine("BookName:- " + dr.Field<string>("bookName"));
+                Console.WriteLine("BookType:- " + dr.Field<string>("bookType"));
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,35 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook System Using LinQ");
+
+            // UC1 Create a new table
+            AddressBookTable addressBookTable = new AddressBookTable();
+
+            // UC3
+            //addressBookTable.GetAllContacts();
+
+            // UC4
+            //addressBookTable.EditExistingContact("Mandara", "M", "PhoneNumber", "9977665543");
+
+            // UC5
+            // addressBookTable.DeleteContact("Vishal", "Singh"); 
+
+            // UC6
+            // addressBookTable.RetrieveByCityOrState("Banglore", "Karnataka"); 
+
+            // UC7
+            // addressBookTable.CountByCityOrState("Banglore", "Karnataka");
+
+            // UC8
+            // addressBookTable.SortedContactsByNameForAgivenCity("Banglore");
+
+            // UC9
+            // addressBookTable.GetAllContacts();
+
+            // UC10
+            // addressBookTable.GetCountByAddressBookType();
+
+            addressBookTable.RetrieveByUsingBookType("Friend");
         }
     }
 }
